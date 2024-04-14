ead5b2d39d4bb73fbb7347c2673d3beba9557f00
Michał Gąsiorek <mgasiorek1998@gmail.com>
2024-04-14 19:03:10 +0200

    build: add github pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mGasiorek1998.github.io',
   integrations: [tailwind()],
 });
